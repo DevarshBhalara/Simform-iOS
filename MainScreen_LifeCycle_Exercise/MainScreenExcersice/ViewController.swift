c0b7caba7aec393aa0c0b79b39e422d8e1624a69
DevarshBhalara <devarshbhalara3072>
2023-02-16 18:03:55 +0530

    Main Screen and ViewController LifeCycle

---
+++ MainScreen_LifeCycle_Exercise/MainScreenExcersice/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  MainScreenExcersice
+//
+//  Created by Devarsh Bhalara on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        
+        debugPrint("load View called")
+        super.loadView()
+        creatlabel()
+    }
+    
+    func creatlabel(){
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+        label.text = "Hello"
+        self.view.addSubview(label)
+        label.center = self.view.center
+        label.backgroundColor = UIColor.blue
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        
+    }
+
+    override func viewDidLoad() {
+        debugPrint("View did Load")
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        debugPrint("View will appear")
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        debugPrint("View will disappear")
+        
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        debugPrint("View did disappear")
+    }
+
+}
