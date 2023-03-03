a81eefd7a56534326dd45362bd5de8e0c9fd74ba
DevarshBhalara <devarshbhalara3072>
2023-03-03 16:38:29 +0530

    Advance Swift Practice

--- DemoProject/DemoProject/first_playground.playground/Pages/Advance_Initializairion_de.xcplaygroundpage/Contents.swift
+++ DemoProject/DemoProject/first_playground.playground/Pages/Advance_Initializairion_de.xcplaygroundpage/Contents.swift
@@ -735,6 +735,42 @@ let chain = Chaining(true)
 print((Chaining(false) ?? chain)?.str?.arr?.last ?? "hi")
 
 
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+            .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And another one: \(generator.random())")
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+
 //delegate
 print()
 struct CalculatorVariable {
