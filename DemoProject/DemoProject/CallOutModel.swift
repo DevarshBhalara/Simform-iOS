ad6bd1f33ae2948734c1d20b715317ce2d7ebdfe
DevarshBhalara <devarshbhalara3072>
2023-04-24 18:18:26 +0530

    completed MKMapView and CLLocation

---
+++ DemoProject/DemoProject/CallOutModel.swift
@@ -0,0 +1,12 @@
+//
+//  CallOutModel.swift
+//  DemoProject
+//
+//  Created by Devarsh Bhalara on 24/04/23.
+//
+
+import Foundation
+struct CallOutModel {
+    var title: String?
+    var detail: String?
+}
---
+++ DemoProject/DemoProject/CustomOverlay.swift
@@ -0,0 +1,15 @@
+//
+//  CustomOverlay.swift
+//  DemoProject
+//
+//  Created by Devarsh Bhalara on 24/04/23.
+//
+
+import Foundation
+import MapKit
+class CustomOverlay: MKPolygon {
+    override init() {
+        super.init()
+       
+    }
+}
